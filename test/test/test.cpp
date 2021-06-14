5a11ca00e15ba508f2255aef6749d825c53e739e
itcast <3025572773@qq.com>
2021-06-14 16:19:39 +0800

    添加

---
+++ test/test/test.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i ,j = 3;
+	for (i = 5 ;i < j; i--)
+	{
+		printf("%d\n", i);
+	}
+	//printf("%d\n", i);
+	system("pause");
+	return 0;
+}
